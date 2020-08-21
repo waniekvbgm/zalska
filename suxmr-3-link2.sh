50a19d42d40f69204414b08977fe853a729af019
waniekvbgm <69979619+waniekvbgm@users.noreply.github.com>
2020-08-21 10:49:40 +0700

    Create suxmr-3-link2.sh

---
+++ suxmr-3-link2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /usr/local/src/xmrig/build/ && ./xmrig --coin=monero -B -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 482EKrrGGGyPoTxmntC9ANKT1bgkQ4N8JTGMqxaFp6oYgMkW4QCy6hNVAnVLytpdKydYPPwuPQp1YBbN22LZz1zyKRysqdD -p x -k
+# cd /usr/local/src/xmrig/build/ && ./miner

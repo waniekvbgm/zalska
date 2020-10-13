e0456da55fab912e79c5df7f46b38945db0276a1
waniekvbgm <69979619+waniekvbgm@users.noreply.github.com>
2020-10-13 22:16:14 +0700

    Update suxmr-1.sh

--- suxmr-1.sh
+++ suxmr-1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update && 
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev gcc g++ make screen curl libgmp-dev automake git &&
+apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libxmu-dev libxi-dev libjansson-dev gcc g++ make screen curl libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=128 &&
 sudo apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev && 

